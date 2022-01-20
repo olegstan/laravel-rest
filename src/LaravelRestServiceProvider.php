7b06cc1c05943c53218a7b7b92a3c8c04545e2fd
olegstan <olegstan@inbox.ru>
2022-01-20 23:42:16 +0300

    created service provider

---
+++ src/LaravelRestServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace LaravelRest;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelRestServiceProvider extends ServiceProvider
+{
+    /**
+     * Boot the service provider.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+
+    }
+}

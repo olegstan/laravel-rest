a3b2a4103897de4da145596e3fc8b27835476962
olegstan <olegstan@inbox.ru>
2025-02-21 01:54:33 +0300

    tests init

---
+++ tests/TestValidation.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class TestValidation extends TestCase
+{
+    public function testExample(): void
+    {
+        $this->assertTrue(true);
+    }
+}

2ac7c51d00ec6dbcecac246d295ca67ee19944c2
olegstan <olegstan@inbox.ru>
2025-12-01 15:43:02 +0300

    default request param

--- src/Http/Controllers/RestController.php
+++ src/Http/Controllers/RestController.php
@@ -110,53 +110,56 @@ abstract class RestController
      * RestController constructor.
      * @throws Exception
      */
-    public function __construct($request)
+    public function __construct($request = null)
     {
-        $query = method_exists($request, 'getQuery') ? $request->getQuery() : [];
-        // Если задана модель, подготавливаем её для работы
-        if ($this->modelName) {
-            $this->restQueryService = new RestQueryService(); // >>> Сохраняем в свойство
+        if($request)
+        {
+            $query = method_exists($request, 'getQuery') ? $request->getQuery() : [];
+            // Если задана модель, подготавливаем её для работы
+            if ($this->modelName) {
+                $this->restQueryService = new RestQueryService(); // >>> Сохраняем в свойство
 
-            $this->restQueryService->addAvailableMethods($this->getBuilderAvailableMethod());
+                $this->restQueryService->addAvailableMethods($this->getBuilderAvailableMethod());
 
-            if ($request->input('debug')) {
-                $this->debug = true;
+                if ($request->input('debug')) {
+                    $this->debug = true;
+                }
+
+                if ($request->has('page')) {
+                    \Illuminate\Pagination\Paginator::currentPageResolver(function () use ($request) {
+                        return $request->input('page');
+                    });
+                }
+
+                $this->model = new $this->modelName;
+
+                // Устанавливаем кол-во записей на странице
+                $this->setPerPage($request->input('perPage'));
+
+                // Считываем query-параметры (ваш метод recursiveUrlDecode может быть где-то в helper)
+                $this->queryBuild = $request->recursiveUrlDecode($request->getQuery(), $request->method());
+
+                // Инициализируем Query Builder
+                $this->modelQuery = $this->modelName::query();
+
+                // >>> Передаём сервису модель и флаг псевдонима
+                $this->restQueryService
+                    ->setModel($this->model)
+                    ->setModelTableAlias($this->modelTableAlias);
+
+                // Прописываем select с учётом псевдонима (если нужно)
+                $this->modelQuery->select($this->restQueryService->withTableAlias('*'));
+
+                // Если нужен alias в секции FROM
+                if ($this->modelTableAlias) {
+                    $this->modelQuery->from($this->model->getTable().' AS '.$this->modelTableAlias);
+                }
+
+                $this->restQueryService->prepareQuery($this->modelQuery, $this->queryBuild);
+
+                // Применяем сортировку по умолчанию
+                $this->defaultOrderBy($request);
             }
-
-            if ($request->has('page')) {
-                \Illuminate\Pagination\Paginator::currentPageResolver(function () use ($request) {
-                    return $request->input('page');
-                });
-            }
-
-            $this->model = new $this->modelName;
-
-            // Устанавливаем кол-во записей на странице
-            $this->setPerPage($request->input('perPage'));
-
-            // Считываем query-параметры (ваш метод recursiveUrlDecode может быть где-то в helper)
-            $this->queryBuild = $request->recursiveUrlDecode($request->getQuery(), $request->method());
-
-            // Инициализируем Query Builder
-            $this->modelQuery = $this->modelName::query();
-
-            // >>> Передаём сервису модель и флаг псевдонима
-            $this->restQueryService
-                ->setModel($this->model)
-                ->setModelTableAlias($this->modelTableAlias);
-
-            // Прописываем select с учётом псевдонима (если нужно)
-            $this->modelQuery->select($this->restQueryService->withTableAlias('*'));
-
-            // Если нужен alias в секции FROM
-            if ($this->modelTableAlias) {
-                $this->modelQuery->from($this->model->getTable().' AS '.$this->modelTableAlias);
-            }
-
-            $this->restQueryService->prepareQuery($this->modelQuery, $this->queryBuild);
-
-            // Применяем сортировку по умолчанию
-            $this->defaultOrderBy($request);
         }
     }
 
